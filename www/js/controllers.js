8a64b3d72e8ca99e94b2dd3dd8b2818c949c3922
Mohsen <mh@Mohsens-MacBook-Pro.local>
2016-02-14 13:42:03 +0100

    initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,10 @@
+angular.module('app.controllers', [])
+  
+.controller('addReceiptCtrl', function($scope) {
+
+})
+   
+.controller('historyCtrl', function($scope) {
+
+})
+    
---
+++ www/js/routes.js
@@ -0,0 +1,51 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+      
+        
+    .state('tabsController.addReceipt', {
+      url: '/add_receipt',
+      views: {
+        'tab4': {
+          templateUrl: 'templates/addReceipt.html',
+          controller: 'addReceiptCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('tabsController.history', {
+      url: '/history',
+      views: {
+        'tab5': {
+          templateUrl: 'templates/history.html',
+          controller: 'historyCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+    .state('tabsController', {
+      url: '/page7',
+      abstract:true,
+      templateUrl: 'templates/tabsController.html'
+    })
+      
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/page7/add_receipt');
+
+});
